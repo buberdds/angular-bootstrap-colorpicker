0cca8877c3a00f770c1a75519d523fd045141c56
Michal Zielenkiewicz <michziel@gmail.com>
2013-09-16 21:17:59 +0200

    upd to karma 0.10

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,18 +1,25 @@
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'libs/jquery-1.10.1.min.js',
-  'libs/angular.min.js',
-  'libs/angular-mocks.js',
-  '../js/**/*.js',
-  'unit/**/*.js'
-];
+module.exports = function(config){
+  config.set({
+    basePath : '',
+    files : [
+      'libs/jquery-1.10.1.min.js',
+      'libs/angular.min.js',
+      'libs/angular-mocks.js',
+      '../js/**/*.js',
+      'unit/**/*.js'
+    ],
+    autoWatch : true,
+    frameworks: ['jasmine'],
+    browsers : ['Chrome'],
+    plugins : [
+      'karma-junit-reporter',
+      'karma-chrome-launcher',
+      'karma-jasmine'
+    ],
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
 
-autoWatch = true;
-
-browsers = ['Chrome'];
-
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
+  })
 };
